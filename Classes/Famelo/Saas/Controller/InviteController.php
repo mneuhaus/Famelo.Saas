17931ce54e770e6b3682149a685083a666a6889d
Marc Neuhaus <mneuhaus@famelo.com>
2014-03-10 10:17:05 +0100

    Added InviteController

---
+++ Classes/Famelo/Saas/Controller/InviteController.php
@@ -0,0 +1,45 @@
+<?php
+namespace Famelo\Saas\Controller;
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class InviteController extends \TYPO3\Flow\Mvc\Controller\ActionController {
+
+	/**
+	 * @var \TYPO3\Flow\Security\Policy\PolicyService
+	 * @Flow\Inject
+	 */
+	protected $policyService;
+
+	/**
+	 *
+	 * @param \Famelo\Saas\Domain\Model\User $user
+	 * @return string
+	 */
+	public function indexAction($user) {
+		$user->addAccount(new \TYPO3\Flow\Security\Account());
+		$this->view->assign('team', $user->getTeam());
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function updateAction() {
+		$teams = $this->request->getInternalArgument('__objects');
+		$role = $this->policyService->getRole('Famelo.Saas:Customer');
+		foreach ($teams as $team) {
+			$account = $team->getMainUser()->getAccount();
+            $account->setAuthenticationProviderName('SaasProvider');
+            $account->addRole($role);
+			$this->persistenceManager->update($team);
+		}
+		// $user->addAccount(new \TYPO3\Flow\Security\Account());
+		// $this->view->assign('team', $user->getTeam());
+	}
+}
+
+?>
